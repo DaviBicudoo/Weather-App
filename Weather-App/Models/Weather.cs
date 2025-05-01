d1cba64cc104dd5727e62c546c3b664b419d6c55
Davi <Davi@DESKTOP-LI13EEL>
2025-05-01 11:13:24 -0300

    Data Service and OpenWeatherAPI consumption

--- Weather-App/Models/Weather.cs
+++ Weather-App/Models/Weather.cs
@@ -2,16 +2,16 @@
 {
     public class Weather
     {
-        public double Longitude { get; set; }
-        public double Latitutde { get; set; }
-        public int Visibility { get; set; }
-        public double MaxTemperature { get; set; }
-        public double MinTemperature { get; set; }
-        public double Temperature { get; set; }
-        public int Sunrise { get; set; }
-        public int Sunset { get; set; }
+        public double? Longitude { get; set; }
+        public double? Latitude { get; set; }
+        public int? Visibility { get; set; }
+        public double? MaxTemperature { get; set; }
+        public double? MinTemperature { get; set; }
+        public double? Temperature { get; set; }
+        public string? Sunrise { get; set; }
+        public string? Sunset { get; set; }
         public string? Main { get; set; }
         public string? Description { get; set; }
-        public double WindSpeed { get; set; }
+        public double? WindSpeed { get; set; }
     }
 }
