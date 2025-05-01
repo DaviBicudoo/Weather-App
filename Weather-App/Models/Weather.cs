72c2216ed38fdefc3598566758a47a211afc84c2
Davi <Davi@DESKTOP-LI13EEL>
2025-05-01 10:35:33 -0300

    Weather.cs model

--- Weather-App/Models/Weather.cs
+++ Weather-App/Models/Weather.cs
@@ -1,12 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Weather_App.Models
+﻿namespace Weather_App.Models
 {
-    internal class Weather
+    public class Weather
     {
+        public double Longitude { get; set; }
+        public double Latitutde { get; set; }
+        public int Visibility { get; set; }
+        public double MaxTemperature { get; set; }
+        public double MinTemperature { get; set; }
+        public double Temperature { get; set; }
+        public int Sunrise { get; set; }
+        public int Sunset { get; set; }
+        public string? Main { get; set; }
+        public string? Description { get; set; }
+        public double WindSpeed { get; set; }
     }
 }
